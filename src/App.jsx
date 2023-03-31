7afc690d8aa15e1117b54c08587776c5c3634e9d
Aram Limpens <a.limpens@amsterdam.nl>
2023-03-31 14:12:43 +0200

    Simplify example

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,20 @@
 import { useLayoutEffect, useRef, useState } from 'react'
 import * as Styled from './App.style'
 
-const descriptions = {
-  current:
-    'Huidige implementatie op onderzoek.amsterdam.nl. Typografie schaalt linear van 320px schermbreedte naar 1920px schermbreedte.',
-  suggested:
-    'Implementatie zoals beschreven in tabel van Fabrique. Typografie schaalt getrapt, linear van 375px schermbreedte naar 819px, en dan weer linear van 820px naar 1920px. Line height klapt om bij breekpunt? Of schaalt ook?',
-}
-
-const paddingDesc = {
-  static:
-    'Ruimte binnen componenten (padding) is statisch, in stappen van 8px. Is alleen te zien bij de button.',
-  relative:
-    'Ruimte binnen componenten (padding) is relatief aan de font size van de tekst in het component. Is alleen te zien bij de button.',
-}
-
-const marginDesc = {
-  static:
-    'Ruimte tussen componenten (margin) is statisch, in stappen van 8px. Na het breekpunt verandert de margin in 1x van de ene naar de andere waarde.',
-  relative:
-    'Ruimte tussen componenten (margin) schaalt linear van de kleinste naar de grootste waarde. Tussen de paragrafen, onder de button en in de card zie je geen verschil, want de margins blijven daar hetzelfde.',
-}
-
 const getCurrentFontSize = (ref) =>
   window.getComputedStyle(ref, null).getPropertyValue('font-size')
 
 function App() {
-  const [selected, setSelected] = useState('current')
-  const [paddingSelected, setPaddingSelected] = useState(false)
-  const [marginSelected, setMarginSelected] = useState(false)
   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
   const [typoSizes, setTypoSizes] = useState({
     h1: 0,
     intro: 0,
     paragraph: 0,
-    button: 0,
   })
 
   const headingRef = useRef()
   const introRef = useRef()
   const paragraphRef = useRef()
-  const buttonRef = useRef()
 
   useLayoutEffect(() => {
     const getSizes = () => {
@@ -50,7 +24,6 @@ function App() {
         h1: getCurrentFontSize(headingRef.current),
         intro: getCurrentFontSize(introRef.current),
         paragraph: getCurrentFontSize(paragraphRef.current),
-        button: getCurrentFontSize(buttonRef.current),
       })
     }
 
@@ -59,7 +32,6 @@ function App() {
         h1: getCurrentFontSize(headingRef.current),
         intro: getCurrentFontSize(introRef.current),
         paragraph: getCurrentFontSize(paragraphRef.current),
-        button: getCurrentFontSize(buttonRef.current),
       })
     }
 
@@ -68,7 +40,7 @@ function App() {
     return () => {
       window.removeEventListener('resize', getSizes)
     }
-  }, [selected])
+  }, [])
 
   return (
     <>
@@ -77,39 +49,6 @@ function App() {
           <h1>Meta</h1>
         </Styled.MetadataTitleContainer>
         <Styled.MetadataContainer>
-          <label htmlFor="option">Kies een optie </label>{' '}
-          <select
-            name="opties"
-            id="option"
-            value={selected}
-            onChange={(e) => setSelected(e.target.value)}
-          >
-            <option value="current">Huidige implementatie</option>
-            <option value="suggested">Implementatie Fabrique</option>
-          </select>
-          <p>{descriptions[selected]}</p>
-          <label htmlFor="padding">
-            Relatieve spacing binnen componenten (spacing)
-          </label>
-          <input
-            type="checkbox"
-            id="padding"
-            name="padding"
-            checked={paddingSelected}
-            onChange={() => setPaddingSelected(!paddingSelected)}
-          />
-          <p>{paddingDesc[paddingSelected ? 'relative' : 'static']}</p>
-          <label htmlFor="margin">
-            Relatieve spacing tussen componenten (margin)
-          </label>
-          <input
-            type="checkbox"
-            id="margin"
-            name="margin"
-            checked={marginSelected}
-            onChange={() => setMarginSelected(!marginSelected)}
-          />
-          <p>{marginDesc[marginSelected ? 'relative' : 'static']}</p>
           <p>{`Schermbreedte: ${screenWidth}px`}</p>
           <p>Fontgroottes:</p>
           <dl>
@@ -119,23 +58,13 @@ function App() {
             <dd>{typoSizes.intro}</dd>
             <dt>Paragraph</dt>
             <dd>{typoSizes.paragraph}</dd>
-            <dt>Button</dt>
-            <dd>{typoSizes.button}</dd>
           </dl>
         </Styled.MetadataContainer>
         <Styled.HeadingAndIntroContainer>
-          <Styled.Heading1
-            option={selected}
-            ref={headingRef}
-            marginSelected={marginSelected}
-          >
+          <Styled.Heading1 ref={headingRef}>
             Voorlopige uitslag Provinciale Statenverkiezingen 2023
           </Styled.Heading1>
-          <Styled.Intro
-            option={selected}
-            ref={introRef}
-            marginSelected={marginSelected}
-          >
+          <Styled.Intro ref={introRef}>
             GroenLinks blijft in Amsterdam de grootste partij. Dat blijkt na het
             tellen van 100 procent van de stemmen. In deze voorlopige uitslag
             van de verkiezingen voor de Provinciale Staten staat de partij op
@@ -144,7 +73,7 @@ function App() {
           </Styled.Intro>
         </Styled.HeadingAndIntroContainer>
         <Styled.TextContainer>
-          <Styled.Paragraph option={selected} ref={paragraphRef}>
+          <Styled.Paragraph ref={paragraphRef}>
             De verkiezingen zijn, landelijk gezien, uitgelopen op aanzienlijke
             verschuivingen in electorale voorkeur, met name aan de
             rechts-conservatieve kant van het politieke spectrum. BBB is de
@@ -152,52 +81,13 @@ function App() {
             PVV verliezen; het CDA verliest bijna de helft van zijn electoraat,
             maar ook SGP en CU verliezen.
           </Styled.Paragraph>
-          <Styled.Paragraph option={selected}>
+          <Styled.Paragraph>
             Aan de linkerkant van het politiek spectrum zijn de verschuivingen
             veel minder groot. GroenLinks en PvdA blijven gezamenlijk ongeveer
             even groot. D66 en SP verliezen licht terwijl de Partij voor de
             Dieren en Volt juist kiezers winnen.
           </Styled.Paragraph>
-          <Styled.Button
-            option={selected}
-            ref={buttonRef}
-            paddingSelected={paddingSelected}
-          >
-            Download PDF (1 MB)
-          </Styled.Button>
         </Styled.TextContainer>
-        <Styled.Card option={selected}>
-          <Styled.CardText option={selected}>Artikel</Styled.CardText>
-          <Styled.CardHeading option={selected}>
-            Onveiligheid toegenomen in 2022
-          </Styled.CardHeading>
-          <Styled.CardText option={selected}>
-            De bij de politie geregistreerde criminaliteit is in de
-            Veiligheidsregio Amsterdam-Amstelland gestegen in 2022 ten opzichte
-            van 2021. Dat blijkt uit de update van de Veiligheidsindexen.
-          </Styled.CardText>
-        </Styled.Card>
-        <Styled.Card option={selected}>
-          <Styled.CardText option={selected}>Publicatie</Styled.CardText>
-          <Styled.CardHeading option={selected}>
-            Schuldenmonitor 2022
-          </Styled.CardHeading>
-          <Styled.CardText option={selected}>
-            In 2021 hebben naar schatting ruim twee op de tien Amsterdammers
-            schulden en/of betalingsachterstanden. Voor een op de tien gaat het
-            om problematische schulden.
-          </Styled.CardText>
-        </Styled.Card>
-        <Styled.Card option={selected}>
-          <Styled.CardText option={selected}>Artikel</Styled.CardText>
-          <Styled.CardHeading option={selected}>
-            Toerisme in de Metropoolregio Amsterdam
-          </Styled.CardHeading>
-          <Styled.CardText option={selected}>
-            Waar verblijven toeristen in de Metropoolregio Amsterdam? En hoeveel
-            toeristen bezoeken de regio?
-          </Styled.CardText>
-        </Styled.Card>
       </Styled.Wrapper>
     </>
   )
--- src/App.style.js
+++ src/App.style.js
@@ -1,9 +1,8 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   max-width: 1440px;
   margin: 0 auto;
-  margin-top: 80px;
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: repeat(1, auto);
@@ -45,45 +44,6 @@ export const TextContainer = styled.div`
   }
 `
 
-const currentH1 = css`
-  font-size: clamp(2rem, calc(2rem + 0.03 * (100vw - 20rem)), 5rem);
-  line-height: clamp(
-    2.1875rem,
-    calc(2.1875rem + 0.028125 * (100vw - 20rem)),
-    5rem
-  );
-`
-
-const suggestedH1 = css`
-  font-size: clamp(
-    4.444375rem,
-    4.444375rem + 2.2225 * (100vw - 51.25rem) / 68.75,
-    6.666875rem
-  );
-  line-height: 100%;
-
-  @media screen and (max-width: 819px) {
-    font-size: clamp(
-      2rem,
-      2rem + 2.368125 * (100vw - 23.4375rem) / 27.75,
-      4.368125rem
-    );
-    line-height: 110%;
-  }
-`
-
-const currentMarginH1 = css`
-  margin-bottom: 40px;
-
-  @media screen and (max-width: 819px) {
-    margin-bottom: 24px;
-  }
-`
-
-const fluidMarginH1 = css`
-  margin-bottom: clamp(1.5rem, calc(1.5rem + 0.01 * (100vw - 20rem)), 2.5rem);
-`
-
 export const Heading1 = styled.h1`
   break-after: avoid;
   break-inside: avoid;
@@ -94,32 +54,37 @@ export const Heading1 = styled.h1`
   margin-inline-end: 0;
   margin-inline-start: 0;
   box-sizing: border-box;
-
-  ${({ option }) => (option === 'current' ? currentH1 : suggestedH1)}
-
-  ${({ marginSelected }) => (marginSelected ? fluidMarginH1 : currentMarginH1)}
-`
-
-const currentIntro = css`
   font-size: clamp(
-    1.375rem,
-    calc(1.375rem + 0.005 * (100vw - 20rem)),
-    1.875rem
+    4.444375rem,
+    4.444375rem + 2.2225 * (100vw - 51.25rem) / 68.75,
+    6.666875rem
   );
-  line-height: clamp(
-    2.1875rem,
-    calc(2.1875rem + 0.00625 * (100vw - 20rem)),
-    2.8125rem
-  );
+  line-height: 100%;
+  margin-bottom: 40px;
+
+  @media screen and (max-width: 819px) {
+    font-size: clamp(
+      2rem,
+      2rem + 2.368125 * (100vw - 23.4375rem) / 27.75,
+      4.368125rem
+    );
+    line-height: 110%;
+    margin-bottom: 24px;
+  }
 `
 
-const suggestedIntro = css`
+export const Intro = styled.p`
+  color: black;
+  margin-block: 0;
+  box-sizing: border-box;
+  -webkit-text-size-adjust: 100%;
   font-size: clamp(
     1.625rem,
     1.625rem + 0.875 * (100vw - 51.25rem) / 68.75,
     2.5rem
   );
   line-height: 150%;
+  margin-bottom: 80px;
 
   @media screen and (max-width: 819px) {
     font-size: clamp(
@@ -128,57 +93,7 @@ const suggestedIntro = css`
       3rem
     );
     line-height: 160%;
-  }
-`
-
-const currentMarginIntro = css`
-  margin-bottom: 80px;
-
-  @media screen and (max-width: 819px) {
     margin-bottom: 56px;
-  }
-`
-
-const fluidMarginIntro = css`
-  margin-bottom: clamp(3.5rem, calc(3.5rem + 0.015 * (100vw - 20rem)), 5rem);
-`
-
-export const Intro = styled.p`
-  color: black;
-  font-size: clamp(
-    1.125rem,
-    calc(1.125rem + 0.00375 * (100vw - 20rem)),
-    1.5rem
-  );
-  margin-block: 0;
-  box-sizing: border-box;
-  -webkit-text-size-adjust: 100%;
-  ${({ marginSelected }) =>
-    marginSelected ? fluidMarginIntro : currentMarginIntro}
-
-  ${({ option }) => (option === 'current' ? currentIntro : suggestedIntro)}
-`
-const currentParagraph = css`
-  font-size: clamp(
-    1.125rem,
-    calc(1.125rem + 0.00375 * (100vw - 20rem)),
-    1.5rem
-  );
-`
-
-const suggestedParagraph = css`
-  font-size: clamp(
-    1.333125rem,
-    1.333125rem + 0.1668750000000001 * (100vw - 51.25rem) / 68.75,
-    1.5rem
-  );
-
-  @media screen and (max-width: 819px) {
-    font-size: clamp(
-      1.125rem,
-      1.125rem + 1.3318750000000001 * (100vw - 23.4375rem) / 27.75,
-      2.456875rem
-    );
   }
 `
 
@@ -190,19 +105,6 @@ export const Paragraph = styled.p`
   -webkit-text-size-adjust: 100%;
   margin-bottom: 40px;
   line-height: 160%;
-  ${({ option }) =>
-    option === 'current' ? currentParagraph : suggestedParagraph}
-`
-
-const currentButton = css`
-  font-size: clamp(
-    1.125rem,
-    calc(1.125rem + 0.00375 * (100vw - 20rem)),
-    1.5rem
-  );
-`
-
-const suggestedButton = css`
   font-size: clamp(
     1.333125rem,
     1.333125rem + 0.1668750000000001 * (100vw - 51.25rem) / 68.75,
@@ -216,103 +118,4 @@ const suggestedButton = css`
       2.456875rem
     );
   }
-`
-
-export const Button = styled.button`
-  line-height: 160%;
-  background-color: rgb(0, 70, 153);
-  color: white;
-  padding: 8px 16px;
-  font-weight: 400;
-  white-space: nowrap;
-  display: inline-flex;
-  -webkit-box-align: center;
-  align-items: center;
-  text-decoration: none;
-  border: none;
-  cursor: pointer;
-  margin-top: 0px;
-  margin-bottom: 120px;
-
-  ${({ option }) => (option === 'current' ? currentButton : suggestedButton)}
-  ${({ paddingSelected }) => paddingSelected && `padding: 0.3333em 0.6666em;`}
-`
-
-export const Card = styled.a`
-  display: block;
-  height: fit-content;
-  text-decoration: none;
-  overflow: hidden;
-  grid-area: auto / auto / auto / span 4;
-  margin-bottom: 40px;
-
-  @media screen and (max-width: 819px) {
-    grid-template-columns: 1fr;
-  }
-`
-
-const currentCardHeading = css`
-  font-size: clamp(
-    1.125rem,
-    1.125rem + (0.375 * (100vw - 20rem)) / 100,
-    1.5rem
-  );
-  line-height: clamp(
-    1.5625rem,
-    1.5625rem + (0.375 * (100vw - 20rem)) / 100,
-    1.9375rem
-  );
-`
-
-const suggestedCardHeading = css`
-  font-size: clamp(
-    1.333125rem,
-    1.333125rem + 0.1668750000000001 * (100vw - 51.25rem) / 68.75,
-    1.5rem
-  );
-  line-height: 120%;
-
-  @media screen and (max-width: 819px) {
-    font-size: clamp(
-      1.125rem,
-      1.125rem + 1.3318750000000001 * (100vw - 23.4375rem) / 27.75,
-      2.456875rem
-    );
-    line-height: 140%;
-  }
-`
-
-export const CardHeading = styled(Heading1)`
-  margin-bottom: 8px;
-  ${({ option }) =>
-    option === 'current' ? currentCardHeading : suggestedCardHeading}
-`
-
-const currentCardText = css`
-  font-size: clamp(1rem, 1rem + (0.125 * (100vw - 20rem)) / 100, 1.125rem);
-  line-height: clamp(
-    1.5rem,
-    1.5rem + (0.3125 * (100vw - 20rem)) / 100,
-    1.8125rem
-  );
-`
-
-const suggestedCardText = css`
-  font-size: clamp(1rem, 1rem + 0.5 * (100vw - 51.25rem) / 68.75, 1.5rem);
-  line-height: 160%;
-
-  @media screen and (max-width: 819px) {
-    font-size: clamp(
-      1rem,
-      1rem + 1.1837499999999999 * (100vw - 23.4375rem) / 27.75,
-      2.18375rem
-    );
-    line-height: 150%;
-  }
-`
-
-export const CardText = styled(Paragraph)`
-  margin-bottom: 4px;
-  ${({ option }) =>
-    option === 'current' ? currentCardText : suggestedCardText}
 `
